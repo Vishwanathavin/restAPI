5c1bcbc0042c969d78dad6acbd1041483de0ec64
Vishwa <vishwanathavin@gmail.com>
2017-11-18 19:35:28 +0530

    plan updated

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 from flask_pymongo import PyMongo
 
+
 app=Flask(__name__)
-app.config['MONGO_DBNAME'] = 'restdb'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+app.config['MONGO_DBNAME'] = 'vishwa_restDB'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/vishwa_restDB'
 mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+	response = {'message': "Welcome to RESTAPI!"}
+	return jsonify(response)
+
+@app.route('/register', methods=['POST'])
+def register():
+
+    # Get data from form/postman
+
+    input_ = {
+
+      'firstName':request.form.get('firstName'),
+      'lastName':request.form.get('lastName'),
+      'emailId':request.form.get('emailId'),
+      'password':request.form.get('password')
+                    }
+
+    print(input_)
+    # Put in database
+    output = mongo.db.users.insert(input_)
+
+
+    return
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
