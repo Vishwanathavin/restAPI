c4cdb994703d9300fcf179ff40d132b3213169bb
Vishwa <vishwanathavin@gmail.com>
2017-11-18 18:57:16 +0530

    base version

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app=Flask(__name__)
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+mongo = PyMongo(app)
